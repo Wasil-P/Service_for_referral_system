ed03b7f343744e86f99600999a3038026b075663
Wasil P <it.dzemidok@gmail.com>
2024-03-14 10:51:49 +0100

    Create project

---
+++ Referral_system/models.py
@@ -0,0 +1,47 @@
+import random
+import string
+from datetime import datetime, timedelta
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class Users(AbstractUser):
+    email = models.EmailField(null=False)
+    referral_code = models.CharField(max_length=20, unique=True, blank=True)
+
+    class Meta:
+        db_table = "users"
+
+    def __str__(self):
+        return self.username
+
+
+def create_expiration_date():
+    expiration_date = datetime.now() + timedelta(days=10)
+    return expiration_date
+
+
+def generate_referral_code(length=15):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+
+class ReferralCode(models.Model):
+    user = models.OneToOneField(Users, on_delete=models.CASCADE)
+    code = models.CharField(max_length=15, unique=True, default=generate_referral_code)
+    expiration_date = models.DateTimeField(default=create_expiration_date)
+
+    class Meta:
+        db_table = "code"
+
+    def __str__(self):
+        return self.code
+
+
+class ReferralRelationship(models.Model):
+    inviter = models.ForeignKey(Users, related_name='inviter', on_delete=models.CASCADE)
+    invited = models.ForeignKey(Users, related_name='invited', on_delete=models.CASCADE)
+    refer_token = models.ForeignKey(ReferralCode, verbose_name="referral_code", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.inviter}_{self.invited}"
